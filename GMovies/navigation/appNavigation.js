831dec6397c6c08164d69564a6124cab918b8a44
OrtigosaLaura <lauraortigosa378@gmail.com>
2024-11-21 11:10:23 -0300

    kaaaaaaaaaaaaaaaaa

--- GMovies/navigation/appNavigation.js
+++ GMovies/navigation/appNavigation.js
@@ -1,21 +1,21 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeSreen from '../screens/HomeSreen';
-import MovieSreen from '../screens/MovieSreen';
-import PersonSreen from '../screens/PersonSreen';
-import SearchSreen from '../screens/SearchSreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import MovieScreen from '../screens/MovieScreen';
+import PersonScreen from '../screens/PersonScreen';
+import SearchScreen from '../screens/SearchScreen';
 
-const Stack = createNativeStackNavigator;
+const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
   return (
-   <NavigationContainer>
-     <Stack.Navigator>
-        <StackSreen name='Home' options={{headerShown: false}} component={HomeSreen} />
-        <StackSreen name='Movie' options={{headerShown: false}} component={MovieSreen} />
-        <StackSreen name='Person' options={{headerShown: false}} component={PersonSreen} />
-        <StackSreen name='Search' options={{headerShown: false}} component={SearchSreen} />
-     </Stack.Navigator>
-   </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' options={{ headerShown: false }} component={HomeScreen} />
+        <Stack.Screen name='Movie' options={{ headerShown: false }} component={MovieScreen} />
+        <Stack.Screen name='Person' options={{ headerShown: false }} component={PersonScreen} />
+        <Stack.Screen name='Search' options={{ headerShown: false }} component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
